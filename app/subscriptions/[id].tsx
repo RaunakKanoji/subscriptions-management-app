f8744624dec153995f9a0296933e026276881ebb
Raunak Kanoji <92038006+RaunakKanoji@users.noreply.github.com>
2026-04-08 00:24:38 +0530

    feat: project setup, nativewind, routing, tab navigation

--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -1,14 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Link, useLocalSearchParams } from 'expo-router'
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SubscriptionDetails = () => {
     const { id } = useLocalSearchParams<{ id: string }>();
   return (
-    <View>
-      <Text>Subscription Details: {id}</Text>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-xl font-bold text-subscription">Subscription Details: {id}</Text>
       <Link href="/" className="mt-4 rounded bg-primary text-white p-4">Go back</Link>
-    </View>
+    </SafeAreaView>
   )
 }
 
