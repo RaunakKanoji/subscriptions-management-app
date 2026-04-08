182d113dfd0154afa83ab0e2f6cff5c064907394
Raunak Kanoji <92038006+RaunakKanoji@users.noreply.github.com>
2026-04-08 22:53:04 +0530

    feat: fonts

--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
 import { Link, useLocalSearchParams } from 'expo-router'
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
@@ -10,8 +10,8 @@ const SubscriptionDetails = () => {
     const { id } = useLocalSearchParams<{ id: string }>();
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-subscription">Subscription Details: {id}</Text>
-      <Link href="/" className="mt-4 rounded bg-primary text-white p-4">Go back</Link>
+      <Text className="text-5xl font-sans-extrabold text-subscription">Subscription Details: {id}</Text>
+      <Link href="/" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go back</Link>
     </SafeAreaView>
   )
 }
