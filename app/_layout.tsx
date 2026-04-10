5b8b425cf0737fafcc7794cda4b19d5023176b18
Raunak Kanoji <92038006+RaunakKanoji@users.noreply.github.com>
2026-04-10 23:57:17 +0530

    feat: homepage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
-import { SplashScreen,Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "@/global.css";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
